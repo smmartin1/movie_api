abe8d9e2fc08b91b702ab246226322312ea39659
smmartin1 <smarie333m@gmail.com>
2022-05-04 16:43:04 -0400

    Added origins

--- index.js
+++ index.js
@@ -26,7 +26,19 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
-app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'https://fathomless-peak-84165.herokuapp.com', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
