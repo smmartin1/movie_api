0d010ce906bdf3df08e84e67b16856c8d01e2399
smmartin1 <smarie333m@gmail.com>
2022-05-18 14:52:09 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,8 +8,8 @@ const jwt = require('jsonwebtoken'),
 require('./passport');
 
 let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
+  return jwt.sign(users, jwtSecret, {
+    subject: users.Username,
     expiresIn: '7d', //Expiration time length in days
     algorithm: 'HS256' //Encodes the values of the JWT
   });
@@ -18,19 +18,19 @@ let generateJWTToken = (user) => {
 //POST login.
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
+    passport.authenticate('local', { session: false }, (error, users, info) => {
+      if (error || !users) {
         return res.status(400).json({
           message: 'Something is wrong.',
-          user: user
+          user: users
         });
       }
-      req.login(user, { session: false }, (error) => {
+      req.login(users, { session: false }, (error) => {
         if (error) {
           res.send(error);
         }
         let token = generateJWTToken(users.toJSON());
-        return res.json({ user, token });
+        return res.json({ users, token });
       });
     })(req, res);
   });
