95940b7f89a6afb470010d5d852574ee95509519
smmartin1 <smarie333m@gmail.com>
2023-02-07 14:13:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let allowedOrigins = [
 	'https://fathomless-peak-84165.herokuapp.com',
 	'http://localhost:1234',
   'http://localhost:4200',
-  'https://smmartin1.github.io/myFlix-Angular-client'
+  'https://smmartin1.github.io'
 ];
 
 app.use(cors({
